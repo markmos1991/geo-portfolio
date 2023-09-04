bf3a08874b3a1b9e68f762bc6fbee36bdf63b13b
markmos1991 <101826190+markmos1991@users.noreply.github.com>
2023-09-04 15:27:15 +0100

    ordered landing

--- geo-portfolio/components/landing.tsx
+++ geo-portfolio/components/landing.tsx
@@ -17,6 +17,7 @@ const Landing = () => {
             <div className=" max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto grid max-w-lg gap-x-8 gap-y-12 sm:max-w-l sm:gap-x-0 sm:gap-y-2 lg:mx-0 lg:max-w-none sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
                 {Projects.map((project) => (
+                     // eslint-disable-next-line react/jsx-key
             <IconHover key={project.id}> {/* Add the 'key' prop here */}
               <div className="p-0 md:p-4 lg:p-6 aspect-w-4 aspect-h-4">
                 <Link href={project.link}>
